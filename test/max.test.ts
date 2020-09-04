69f1ceaf7a70060e0bd6f8194d9d9e44b0152ff0
Haixiang <haixiang6123@gmail.com>
2020-09-04 23:56:19 +0800

    feat: 添加复杂对象的测试

--- test/max.test.ts
+++ test/max.test.ts
@@ -1,11 +1,20 @@
 import max from '../src/max'
 
 describe('max', () => {
-  it('返回最大值', () => {
+  it('返回简单元素最大值', () => {
     const array = [1, 2, 3, 4, 5]
 
     const result = max(array, (a, b) => a > b)
 
     expect(result).toEqual(5)
+  })
+  it('返回复杂对象的最大值', () => {
+    const array = [
+      {value: 1}, {value: 2}, {value: 3}
+    ]
+
+    const result = max(array, (a, b) => a.value > b.value)
+
+    expect(result).toEqual({value: 3})
   })
 })
